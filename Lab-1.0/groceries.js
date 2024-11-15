9b73294f626b410948506e8fad20a7a289da9988
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2024-11-15 09:06:57 +0000

    Put various blocks of initial code in Groceries List into functions

--- Lab-1.0/groceries.js
+++ Lab-1.0/groceries.js
@@ -1,69 +1,103 @@
-//needed modules
+//Importing neccesary modules
 const fs = require('fs')
 const path = require("path")
 
-//data file paths resolution
+
+//getting path to data files
 const groceryFilePath = path.resolve(__dirname,'grocery_list.json')
 const priceFilePath = path.resolve(__dirname,'priceList.json')
 
-let priceData //to read price file
-let itemData // to read items file
-let prices //to hold parsed json data
-let items // to hold parsed items
+
 let totalCost = 0 // to accumulate costs from different items
 let output = ['Grocery List:','--------------------------------'] // Holds output strings format
 
 
-
-try {
-    priceData = fs.readFileSync(priceFilePath, 'utf-8')
-    itemData = fs.readFileSync(groceryFilePath, 'utf-8')
-    prices = JSON.parse(priceData)
-    const itemsData = JSON.parse(itemData)
-    items = itemsData.items
-
-} catch (error) {
-    console.log(`Couldn't read or parse data file/s`,error.name)
+/**
+ * 
+ * @param {strin} filepath - path to the data file that has the data we want to read
+ * @returns {Array} - read data
+ */
+const readFile = filepath =>{
+    try {
+        return fs.readFileSync(filepath, 'utf-8')
+    } catch (error) {
+        console.log(`Couldn't read the ${filepath} file`)
+        return null;
+    }
 }
 
 
+/**
+ * 
+ * @param {object} data - Data to be converted to JSON object
+ * @returns {object} - JSON version of the parsed data
+ */
+const parseData = data =>{
+    try {
+        return JSON.parse(data)
+    } catch (error) {
+        console.log("Coulsn't parse JSON data", error)
+        return null;
+    }
+}
 
+
+/**
+ * 
+ * @param {string} item - Item we want to get it's total cost
+ * @param {number} price - Price of th item
+ * @returns {number} - product of quantity of the item and it's price
+ */
 const getCost = (item, price) =>{
-
     if (!price){
         console.log(`Price for ${item.name} not found`)
     }
-
     return item.quantity * price
 }
 
 
+/**
+ * 
+ * @param {string} fileName 
+ * @param {Array} data 
+ * @returns - void
+ */
+const generateReceipt = (fileName,data) => {
+    try {
+        fs.writeFileSync(`./${fileName}`, data);
+        console.log(`Data successfully written to ${fileName}`);
+    } catch (error) {
+        console.error(`Couldn't write to the file ${fileName}`, error);
+    }
+}
 
 
+//Reading raw data 
+const priceData = readFile(priceFilePath)
+const itemData = readFile(groceryFilePath)
+
+
+//Parsing read data to JSON objects to work with
+const prices = parseData(priceData)
+const items = parseData(itemData).items
+
+
+
+
+
+//Iterating through all items in the grocery list to get cost, and add it's cost to the grand total
 items.forEach( item =>{
-    
-    // if (!price){
-    //     console.log(`${item.name} does not have a price`)
-    //     return
-    // }
-    // const quantity = item.quantity
-    // const cost = item.quantity * price
     const cost = getCost(item, prices[item.name])
     totalCost += cost
-
     output.push(`${item.name} - ${item.quantity} ${item.unit} - ${cost}`)
 }
 )
 
 
-//Last two lines of the output
+//Pushing last two lines of the output format to the output strings collector
 output.push('--------------------------------');
 output.push(`Total: $${totalCost.toFixed(2)}`);
 
 
-//Writing to result as the output file
-try {
-    fs.writeFileSync('./shopping_receipt.txt', output.join('\n'))
-} catch (error) {
-    console.log(`Couldn't wirte to the file`, error.name)
-}
+//Writing the output string collector elements to the receipt
+generateReceipt('shopping_receipt.txt',output.join('\n'))
